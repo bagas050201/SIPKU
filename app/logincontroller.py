142dc9e211cde66e6c068004bc28a80fe0e4fd82
Muhammad Bagas Pradana <54340164+bagas050201@users.noreply.github.com>
2021-01-07 17:03:14 +0700

    Update logincontroller.py

--- app/logincontroller.py
+++ app/logincontroller.py
@@ -1,32 +1,43 @@
 from flask import Flask, render_template, url_for, redirect, request, session, Blueprint
 from bson.objectid import ObjectId
 from model.member import Member
+from model.log import Log
 
-
-login=Blueprint('login', __name__, url_prefix='/login')
+login = Blueprint('login', __name__, url_prefix='/login')
 
 @login.route('/')
 def login_user():
-    if('masuk' in session):
+    if ('masuk' in session):
         return redirect(url_for("dashboard.dashboard_user"))
+    if ('admin' in session):
+        return redirect(url_for("dashboard.dashboard_admin"))
     return render_template("login.html")
+
 
 @login.route('/auth', methods=["POST", "GET"])
 def authLogin():
-    if request.method=='POST':
-        id_member = int(request.form['idMember'])
+    if request.method == 'POST':
+        id_member = request.form['idMember']
         password = request.form['password']
-        if checkLogin(id_member, password):
-            session['masuk']=id_member
+
+        if id_member == '2021' and password == "admin":
+            session['admin'] = id_member
+            return redirect(url_for('dashboard.dashboard_admin'))
+        elif checkLogin(id_member, password):
+            session['masuk'] = int(id_member)
+            Log().addLog(Member().getFullname(str(session['masuk'])) +
+                         " login")
             return redirect(url_for('dashboard.dashboard_user'))
+
     return url_for('login.login_user')
 
-def checkLogin(id_member, password):
-    member=Member()
-    if member.getPassword(id_member)==password:
-        return True
-    # return False
-    return redirect(url_for('dashboard.dashboard_user'))
 
-# print(checkLogin(12345, "sesuatu"))
-# print(session['masuk'])
+def checkLogin(id_member, password):
+    member = Member()
+    print("masuk ga ya")
+    print(member.getPassword(id_member))
+    # print(password)
+    print("test")
+    if str(member.getPassword(id_member)) == password:
+        return True
+    return False
